155fe37f182d42461c75e6bef31f3dea91dfd162
ViktorStoichev <viktor.stoichev888@abv.bg>
2025-07-09 11:13:12 +0300

    Update App.tsx

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,16 +2,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AssetPage from "./pages/AssetPage";
 import Dashboard from "./pages/Dashboard";
 import HomePage from "./pages/HomePage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import AboutPage from "./pages/AboutPage";
 
 export default function App() {
-  return (<>
+  return (
     <Router>
+      <div className="flex flex-col min-h-screen bg-gray-50">
+        <Header />
+        <main className="flex-1">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/asset/:id" element={<AssetPage />} />
+            <Route path="/about" element={<AboutPage />} />
       </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
-  </>
   );
 }
