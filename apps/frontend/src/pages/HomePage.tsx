8f50c1cae69a50b5c9f7a9d83e089715be4b2b37
ViktorStoichev <viktor.stoichev888@abv.bg>
2025-07-09 11:04:50 +0300

    Add a home page where are shown all of the coins and their current prices

--- apps/frontend/src/pages/HomePage.tsx
+++ apps/frontend/src/pages/HomePage.tsx
@@ -1,11 +1,12 @@
-import { useNavigate } from "react-router-dom";
+import SearchBar from "../components/SearchBar";
+import CryptoList from "../components/CryptoList";
 
-export default function HomePage () {
-    const navigate = useNavigate();
-
-    return (
-        <div>
-            <button onClick={() => navigate('/dashboard')}>Dashboard</button>
-        </div>
-    );
+export default function HomePage() {
+  return (
+    <div className="max-w-6xl mx-auto py-10 px-4">
+      <h1 className="text-4xl font-extrabold mb-6 text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-center drop-shadow-lg">Track Cryptocurrencies in Real Time</h1>
+      <SearchBar />
+      <CryptoList />
+    </div>
+  );
 }
